0c469f0664d473c36e7edb7584bcaf28d8fc2da7
Stelare-Rose@Selene <stelare-rose@noreply.git.starrytea.cc>
2026-09-08 18:30:14 +0700

    Feat: Swapped to using mkDefault

--- common/default.nix
+++ common/default.nix
@@ -1,21 +1,23 @@
-{ config, pkgs, ctx, ...}@inputs:
+{ config, pkgs, ctx, lib, ...}@inputs:
 let 
   # Unwrap ctx here
 in
   {
-  nixpkgs.config.allowUnfree = true;
-  hardware.enableAllFirmware = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.kernelPackages = lib.mkDefault pkgs.linuxKernel.packages.linux_7_2;
 
   imports = [
     ../modules/nixos
   ];
 
-  modules.audio.enable = true;
-  modules.desktop.enable = true;
-  modules.virtualisation.enable = true;
-  modules.user.enable = true;
-  modules.boot.enable = true;
+  modules.audio.enable = lib.mkDefault true;
+  modules.desktop.enable = lib.mkDefault true;
+  modules.virtualisation.enable = lib.mkDefault true;
+  modules.user.enable = lib.mkDefault true;
+  modules.boot.enable = lib.mkDefault true;
 
+  time.timeZone = lib.mkDefault "Asia/Jakarta";
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
 	system.stateVersion = "24.05"; 
 }
