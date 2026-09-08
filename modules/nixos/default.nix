3d8f07762bd5934bc12972ceaab1f844aa3172ce
Stelare-Rose@Selene <stelare-rose@noreply.git.starrytea.cc>
2026-09-08 18:32:52 +0700

    Feat: Swapped to using dynamic imports for all modules

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ lib, ... }:
+  let
+  dir = ./.;
+  entries = builtins.readDir dir;
+
+  isModule = name: type:
+    (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
+    || (type == "directory" && builtins.pathExists (dir + "/${name}/default.nix"));
+
+  moduleNames = builtins.attrNames (lib.filterAttrs isModule entries);
+in
 {
-  imports = [
-    ./virtualisation
-    ./audio
-    ./user
-    ./desktop
-    ./boot
-  ];
+  imports = map (name: dir + "/${name}") moduleNames;
 }
