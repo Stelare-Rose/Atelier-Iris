0a2d1103e7831cb468ae327a503682bf53807171
Stelare-Rose@Crescent <stelare-rose@noreply.git.starrytea.cc>
2026-09-09 14:41:51 +0700

    Wip: Included Old Config for Todo

--- configuration.nix
+++ configuration.nix
@@ -10,27 +10,12 @@
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 	imports =
 		[ # Include the results of the hardware scan.
-			(module + "/environment/Hyprland")
 			(module + "/environment/Utilities/Passwords")
-			(module + "/environment/Utilities/Wayland")
 			(module + "/environment/Utilities/Appimage-Utils")
 			(module + "/environment/Utilities/Images")
-			(module + "/environment/Productivity/adb")
-			(module + "/environment/Productivity/android-emu")
 			(module + "/environment/Zen")
 			inputs.home-manager.nixosModules.default
 		];
-
-  boot.extraModprobeConfig = ''
-    install esp4 ${pkgs.coreutils}/bin/false
-    install esp6 ${pkgs.coreutils}/bin/false
-    install rxrpc ${pkgs.coreutils}/bin/false
-  '';
-  boot.blacklistedKernelModules = [
-    "esp4"
-    "esp6"
-    "rxrpc"
-  ];
 
 
 	#User Account
@@ -54,31 +39,16 @@
 		waybar
     bitwarden-cli
 		godot_4
-		dbeaver-bin
-		sqlcmd
 		brightnessctl
 		stow
-		openssl
 		rmpc
-		transmission_4-gtk
-		signal-desktop
 		via
-		temurin-bin-21
-		unstable.zoom-us
 		dotool
-		mysql84
 		yazi
-		unstable.opencode
 		self.packages.${pkgs.system}.pyxis
 		nautilus
-    posting
-    cloudflared
     mendeley
-    android-tools
-    tokei
-    jq
     ddcutil
-    unstable.yt-dlp
     whisper-cpp
     inputs.horologium.packages.${pkgs.system}.horologium-cli
 		];
@@ -99,22 +69,5 @@
 
 	
 	# MySQL Daemon
-	services.mysql = {
-		enable = true;
-		package = pkgs.mysql84;
-	};
 	system.stateVersion = "24.05"; 
-
-	programs.nix-ld.enable = true;
-	programs.nix-ld.libraries = with pkgs; [
-		xorg.libICE
-		xorg.libSM
-		xorg.libX11
-		icu
-		fontconfig
-	];
-
-
 }
-
-
