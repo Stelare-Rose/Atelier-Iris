d75fb415dfb4e9dc8fd751aea7a06dd7e5fa9652
Stelare-Rose@Crescent <stelare-rose@noreply.git.starrytea.cc>
2026-09-10 10:36:57 +0700

    Feat: Added Plymouth And Adjusted Bootloader Settings

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -5,17 +5,39 @@ in
   {
   options.modules.boot.enable = lib.mkEnableOption "bootloader (GRUB)";
   config = lib.mkIf cfg.enable {
-    boot.loader = {
-      efi.canTouchEfiVariables = true;
-      efi.efiSysMountPoint = "/boot/efi";
-      timeout = 30;
-      grub = {
+    systemd.services.plymouth-quit = {
+      serviceConfig.ExecStart = lib.mkForce "${pkgs.plymouth}/bin/plymouth --wait quit";
+    };
+    boot = {
+      plymouth = {
         enable = true;
-        efiSupport = true;
-        device = "nodev";
-        useOSProber = true;
-        theme = (pkgs.catppuccin-grub.override {flavor = "latte";});
+        theme = "breeze"; # Placeholder, Custom in Progress
       };
-    }; 
+
+      consoleLogLevel = 3;
+      initrd = {
+        systemd.enable = true;
+        verbose = false;
+      };
+      kernelParams = [
+        "quiet"
+        "splash"
+        "rd.udev.log_level=3"
+        "rd.systemd.show_status=auto"
+      ];
+      loader = {
+        efi.canTouchEfiVariables = true;
+        efi.efiSysMountPoint = lib.mkIf (!config.virtualisation.useBootLoader) "/boot/efi";
+        timeout = 0;
+        grub = {
+          enable = true;
+          efiSupport = true;
+          device = "nodev";
+          useOSProber = true;
+          theme = (pkgs.catppuccin-grub.override {flavor = "latte";});
+          splashImage = null;
+        };
+      }; 
+    };
   };
 }
