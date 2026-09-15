9c3b47159459a3c3a82b8e4b383f9910d9e2908f
Stelare-Rose@Crescent <stelare-rose@noreply.git.starrytea.cc>
2026-09-15 12:36:16 +0700

    Fix: Changed Default Disk Size for Virtualization

--- common/virtual-default.nix
+++ common/virtual-default.nix
@@ -5,12 +5,14 @@
       virtualisation = {
         memorySize = lib.mkDefault 2048;
         cores = lib.mkDefault 4;
+        diskSize = lib.mkDefault 32768;
       };         
     };
     vmVariantWithBootLoader = {
       virtualisation = {
         memorySize = lib.mkDefault 2048;
         cores = lib.mkDefault 4;
+        diskSize = lib.mkDefault 32768;
       };
     };
   };
