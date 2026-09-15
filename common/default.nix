ecf915ff1e5a636b6a5074cd238479804a206391
Stelare-Rose@Crescent <stelare-rose@noreply.git.starrytea.cc>
2026-09-15 11:14:36 +0700

    Feat: Added Default Settings for All Modules in Common

--- common/default.nix
+++ common/default.nix
@@ -18,12 +18,32 @@ in
 		variant = "";
 	};
 
-  modules.audio.enable = lib.mkDefault true;
-  modules.desktop.enable = lib.mkDefault true;
-  modules.virtualisation.enable = lib.mkDefault true;
-  modules.user.enable = lib.mkDefault true;
-  modules.boot.enable = lib.mkDefault true;
-
+  modules = {
+    audio.enable = lib.mkDefault true;
+    boot.enable = lib.mkDefault true;
+    browser.enable = lib.mkDefault true;
+    communication.enable = lib.mkDefault true;
+    constellation.enable = lib.mkDefault true;
+    creative.enable = lib.mkDefault true;
+    desktop.enable = lib.mkDefault true;
+    development.enable = lib.mkDefault true;
+    downloaders.enable = lib.mkDefault true;
+    fonts.enable = lib.mkDefault true;
+    gaming.enable = lib.mkDefault true;
+    media = {
+      viewing.enable = lib.mkDefault true;
+      production.enable = lib.mkDefault true;
+    };
+    networking.enable = lib.mkDefault true;
+    nix-ld.enable = lib.mkDefault true;
+    obs.enable = lib.mkDefault true;
+    office.enable = lib.mkDefault true;
+    shell.enable = lib.mkDefault true;
+    user.enable = lib.mkDefault true;
+    utils.enable = lib.mkDefault true;
+    virtualisation.enable = lib.mkDefault true;
+  };
+    
   time.timeZone = lib.mkDefault "Asia/Jakarta";
   i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
 	system.stateVersion = "24.05"; 
