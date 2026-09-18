5bc38ebf812004ed811524f69556844363910a4c
Stelare-Rose@Selene <stelare-rose@noreply.git.starrytea.cc>
2026-09-18 16:44:59 +0700

    Feat[Selene]: Added Selene Config

--- hosts/selene/default.nix
+++ hosts/selene/default.nix
@@ -1,8 +1,41 @@
-{ ... }:
-{
+{ ctx, pkgs, ... }:
+let
+  root = ctx.root;
+in
+  {
   imports = [
     ../../common/default.nix
     ./hardware.nix
   ];
-  modules.user.extraImports = [ ./users/stelare.nix ];
+	services.xserver.deviceSection = ''
+	    Option "VariableRefresh" "true"
+	'';
+  boot.kernelModules = [
+    "i915"
+    "nfs"
+  ];
+	boot.kernelParams = [ "i915.enable_guc=3" ];
+	hardware.enableRedistributableFirmware = true;
+  networking.hostName = "Selene";
+  sops.age.keyFile = "/home/Stelare/.config/sops/age/keys.txt";
+	hardware.graphics = {
+		enable = true;
+		extraPackages = with pkgs; [
+			intel-media-driver
+			intel-compute-runtime
+			vpl-gpu-rt
+			intel-graphics-compiler
+			level-zero
+		];
+	};
+	services.sunshine = {
+		enable = true;
+		autoStart = true;
+		capSysAdmin = true;	
+    openFirewall = true;
+	};
+  modules = {
+    user.extraImports = [ ./users/stelare.nix ];
+    wireguard.secretPath = root + /secrets/selene/wireguard/wg0.conf;
+  };
 }
