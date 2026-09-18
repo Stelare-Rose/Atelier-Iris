4cb98553f0b5c1b3eb8e6f90a730832e92ed6869
Stelare-Rose@Crescent <stelare-rose@noreply.git.starrytea.cc>
2026-09-18 17:16:19 +0700

    Fix: Moved Update Scripts

--- dotfiles/scripts/update-impure.sh
+++ dotfiles/scripts/update-impure.sh
@@ -1,11 +1,9 @@
 echo "Updating system using flake $(hostname) on $(hostname)"
 
-cd ~/Lunelle
-
-stow -t ~ home
+cd ~/Atelier-Iris
 
 echo "Current Git Status"
 git status
 
 echo "System Update"
-sudo nixos-rebuild switch --flake ~/Lunelle/#$(hostname) --impure
+sudo nixos-rebuild switch --flake ~/Atelier-Iris/#$(hostname) --impure
