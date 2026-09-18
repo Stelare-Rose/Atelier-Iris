a9e6ed244f028bb5b001625a2db0289ec10c7e6e
Stelare-Rose@Crescent <stelare-rose@noreply.git.starrytea.cc>
2026-09-18 16:09:47 +0700

    Fix[Crescent]: Firmware Issue for R680M

--- hosts/crescent/default.nix
+++ hosts/crescent/default.nix
@@ -1,6 +1,6 @@
 { ctx, ... }:
 let
-  root = ctx.root;
+  inputs = ctx.inputs;
 in
   {
   imports = [
@@ -8,8 +8,18 @@ in
     ./hardware.nix
   ];
   networking.hostName = "Crescent"; 
+  # Temporary workaround for bug regarding R680M, swap linux-firmware to the release version.
+  nixpkgs.overlays = [
+    (final: prev: {
+      linux-firmware = (import inputs.nixpkgs-release {
+        system = prev.stdenv.hostPlatform.system;
+        config.allowUnfree = true;
+      }).linux-firmware;
+    })
+  ];
   modules = {
     user.extraImports = [ ./users/stelare.nix ];
-    wireguard.secretPath = root + /secrets/crescent/wireguard/wg0.conf;
+    wireguard.enable = false;
+ #   wireguard.secretPath = root + /secrets/crescent/wireguard/wg0.conf;
   };
 }
